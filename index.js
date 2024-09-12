6a7a6f7da028d9f94c6e57ef80a2a2b463cc3b92
DAIRRENYS <floresdairreny@gmail.com>
2024-09-12 03:29:24 -0500

    inde ca

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const fn = () => {
-    const miString = "soy un string!"
-    let b = miString + " muy feliz"
-    console.log(b)
-}
-
-fn()
-
-const clickeado = () => {
-    let a = "chanchito feliz"
-
-    console.log(a)
-}
+print("Hola mundo!")
